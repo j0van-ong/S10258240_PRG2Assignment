140b5dee03dac9e129ee9647ba334e664753ba78
lucas0eo <150000880+lucas0eo@users.noreply.github.com>
2024-01-15 20:16:45 +0800

    Adding customer, order and pointcard class's

---
+++ PointCard.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace PRG2_FinalAssesment
+{
+    class PointCard
+    {
+        private int points;
+        private int punchCard;
+        private string tier;
+        public int Points {  get; set; }
+        public int PunchCard { get; set; }
+        public string Tier { get; set; }
+        public PointCard() { }
+        public PointCard(int points,int punchCard)
+        {
+            Points = points;
+            Points = punchCard;
+        }
+        void AddPoints(int addedpoints)
+        {
+            Points += addedpoints;
+        }
+        void RedeemPoints(int points)
+        {
+            if (tier == "Gold"||tier == "Silver")
+            {
+                Points -= points;
+            }
+            else
+            {
+                Points = Points;
+            }
+        }
+            void Punch()
+        {
+            int FinalPrice;
+            if (punchCard == 10)
+            {
+                punchCard = 0;
+                FinalPrice = 0;
+            }
+            else
+            {
+                punchCard += 1;
+            }
+        }
+        public override string ToString()
+        {
+            return " Points: " + Points + " PunchCard " + punchCard;
+        }
+    }
+    
+}
